bd7577d8c97e3ecc348e06ed8211ac05c41fb60e
Abduvokh1D <abduvokhideurope@gmail.com>
2025-07-07 14:27:58 +0500

    adding more state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// step-4, just import store here.
+import "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
