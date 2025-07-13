6945e4595985b7bd63c0b72167605d5bbf0f161e
Abduvokh1D <abduvokhideurope@gmail.com>
2025-07-13 12:08:06 +0500

    Professional Redux file structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// step-4, just import store here.
-import "./store";
-
+import store from "./store";
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
